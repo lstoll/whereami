e5421d72512d4089e2185d42c438ddf0a14d0c04
Lincoln Stoll <lstoll@lstoll.net>
2010-05-07 19:02:24 +0200

    Completing basic functionality

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require 'sinatra'
 require 'rack'
 require 'twitter'
 require 'geokit'
+include Geokit::Geocoders
 require 'memcached'
+require 'json'
 
 configure :development do
   Sinatra::Application.reset!
@@ -14,13 +16,35 @@ configure do
 end
 
 def get_location
-  httpauth = Twitter::HTTPAuth.new(ENV['TWITTER_USERNAME'], ENV['TWITTER_PASSWORD'])
-  client = Twitter::Base.new(httpauth)
-  client.user_timeline.each { |tweet| puts tweet.coordinates.inspect if tweet.coordinates }
-  include Geokit::Geocoders
-  # need to switch coord order
-  GoogleGeocoder.reverse_geocode([coord[1], coord[0]])
-  
+  begin
+    CACHE.get 'lstoll-loc-data'
+  rescue Memcached::NotFound
+    # If it's more than 8 hrs old, it's expired
+    expires_in = 8 * 60 * 60
+    puts "Re-loading data from twitter"
+    httpauth = Twitter::HTTPAuth.new(ENV['TWITTER_USERNAME'], ENV['TWITTER_PASSWORD'])
+    client = Twitter::Base.new(httpauth)
+    coords = nil
+    updated = nil
+    page = 0
+    until coords
+      if tweet = client.user_timeline(:page => page).detect { |t| t.coordinates }
+        coords = tweet.coordinates.coordinates
+        updated = tweet.created_at
+      end
+      page += 1
+    end
+    # need to switch coord order
+    require 'pp'
+    loc = GoogleGeocoder.reverse_geocode([coords[1], coords[0]])
+    loc_data = { :city => [loc.city, loc.district,  'N/A'].compact[0],
+      :state => [loc.state, 'N/A'].compact[0],
+      :country => [loc.country, 'N/A'].compact[0],
+      :updated => [updated, 'N/A'].compact[0]
+    }
+    CACHE.set 'lstoll-loc-data', loc_data, expires_in
+    loc_data
+  end
 end
 
 get "/" do
@@ -28,5 +52,5 @@ get "/" do
 end
 
 get "/loc.json" do
-  "hi!"
+  "#{params[:callback]}(#{get_location.to_json});"
 end
